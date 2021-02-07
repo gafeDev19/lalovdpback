31181987d9786b4aca63b027c03f5cc9b9a7c43e
Federico Galvan <federicogalvanap@gmail.com>
2021-02-07 18:53:21 -0300

    Cambio en script de prod y config de DB para deploy

--- src/config/dbConn.js
+++ src/config/dbConn.js
@@ -1,15 +1,22 @@
-import { Pool, types } from 'pg'
-import dotenv from 'dotenv'
+const { Pool, types } = require('pg')
+const dotenv = require('dotenv')
 dotenv.config();
 
 types.setTypeParser(1114, str => str);
 
+//Dev
+// const pool = new Pool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     port: process.env.DB_PORT
+// })
+
+//Prod
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
 })
 
 module.exports = pool
