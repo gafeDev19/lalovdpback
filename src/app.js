990d4e935816b508dfb2049f3efbd02448e350fe
Federico Galvan <federicogalvanap@gmail.com>
2021-02-08 19:47:12 -0300

    Ready to deploy

--- src/app.js
+++ src/app.js
@@ -2,9 +2,9 @@ const https = require('https')
 const fs = require('fs')
 import express from 'express';
 import session from 'express-session';
-import redis from 'redis';
-const client  = redis.createClient();
-const redisStore = require('connect-redis')(session);
+// import redis from 'redis';
+// const client  = redis.createClient();
+// const redisStore = require('connect-redis')(session);
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import cors from 'cors';
@@ -15,32 +15,32 @@ const app = express();
 dotenv.config();
 // Middleware
 app.use(cookieParser());
-app.use(session({
-  secret: '5c539542c5f33a5cbe00eb71265e1ba3',
-  name: '_redisLalo',
-  store: new redisStore({ host: '127.0.0.1', port: 6379, client: client, ttl : 604800}),
-  saveUninitialized: false,
-  resave: false,
-  cookie: { secure: true, maxAge: 604800000, domain: 'lalovdp' }
-}));
+// app.use(session({
+//   secret: '5c539542c5f33a5cbe00eb71265e1ba3',
+//   name: '_redisLalo',
+//   store: new redisStore({ host: '127.0.0.1', port: 6379, client: client, ttl : 604800}),
+//   saveUninitialized: false,
+//   resave: false,
+//   cookie: { secure: true, maxAge: 604800000, domain: 'lalovdp' }
+// }));
 app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.use('/api', require('./src/routes/user'))
-app.use('/api', require('./src/routes/product'))
-app.use('/api', require('./src/routes/budget'))
-app.use('/api', require('./src/routes/order'))
+app.use('/api', require('./routes/user'))
+app.use('/api', require('./routes/product'))
+app.use('/api', require('./routes/budget'))
+app.use('/api', require('./routes/order'))
 
 //Handle production
 if(process.env.NODE_ENV === 'production') {
   //Static folder
-  app.use(express.static(__dirname + '/public'))
+  app.use(express.static(__dirname + '../public'))
 
   // Handle SPA
-  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '../public/index.html'))
 }
 
 
